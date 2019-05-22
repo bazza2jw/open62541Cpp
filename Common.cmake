21e23dc36339567d265c0ecc3b1c5f95ce64cac6
hicklin <venturehicklin@gmail.com>
2019-05-22 17:59:28 +0100

    Simplified the examples' CMakeLists.txt files. Boost is not looked for if found, simplifying the CMake output when using the with_examples flag.

--- Common.cmake
+++ Common.cmake
@@ -6,11 +6,12 @@ set(Boost_USE_STATIC_LIBS OFF)
 set(Boost_USE_MULTITHREADED ON)  
 set(Boost_USE_STATIC_RUNTIME OFF) 
 
-find_package(Boost COMPONENTS REQUIRED system thread)
+if(NOT Boost_FOUND)
+    find_package(Boost COMPONENTS REQUIRED system thread)
+endif()
 
 # add boost to target
 if(Boost_FOUND)
-    include_directories(${BOOST_INCLUDE_DIRS})
     link_directories(${Boost_LIBRARY_DIRS})
     add_definitions(${Boost_DEFINITIONS})
     include_directories(${Boost_INCLUDE_DIRS})  
