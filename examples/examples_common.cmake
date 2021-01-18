441bf7da1580eb0e3d32db8493806e9abd3f397f
Jean-Joël Borter <jeanjoel.borter@gmail.com>
2021-01-18 15:57:51 +0100

    Refactor examples_common.cmake: replace macro-like behavior by add_example() function with argument list expansion. All example CMakeLists.txt modified to use the new add_example() function.

--- examples/examples_common.cmake
+++ examples/examples_common.cmake
@@ -1,9 +1,18 @@
 cmake_minimum_required (VERSION 3.9)
-project(${APPNAME})
 
-# Add Open62541 libraries + all its dependecies (boost)
-add_executable(${APPNAME} ${SOURCES})
-target_link_libraries(${APPNAME} PRIVATE open62541cpp)
-include(../../Common.cmake)
+# Create an example project with the given name and sources
+# The sources are a list of file name separated by space
+# usage 1: add_example(name_of_your_project main.cpp main.h)
+# usage 2: set(SOURCES main.cpp main.h)
+#          add_example(name_of_your_project ${SOURCES})
+function(add_example ex_target)
+    project(${ex_target})
 
-set_build_system_option(${APPNAME})
+    # Add Open62541 libraries + all its dependecies (boost)
+    # ${ARGN} expand the arguments after ex_target to a list
+    add_executable(${ex_target} ${ARGN})
+    target_link_libraries(${ex_target} PRIVATE open62541cpp)
+
+    include(../../Common.cmake)
+    set_build_system_option(${ex_target})
+endfunction()
