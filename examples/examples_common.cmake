64c916db8c84d6adc3483da53a1ccb30c885c1a3
Jean-Joël Borter <jeanjoel.borter@gmail.com>
2021-01-18 16:46:04 +0100

    Refactor CMake: output path changed to "build/bin" for all projects (lib + examples).

--- examples/examples_common.cmake
+++ examples/examples_common.cmake
@@ -6,12 +6,19 @@ cmake_minimum_required (VERSION 3.9)
 # usage 2: set(SOURCES main.cpp main.h)
 #          add_example(name_of_your_project ${SOURCES})
 function(add_example ex_target)
-    project(${ex_target})
-
+    project(       ${ex_target})
+    add_executable(${ex_target} ${ARGN}) # ${ARGN} expand the arguments after ex_target to a list
+    
     # Add Open62541 libraries + all its dependecies (boost)
-    # ${ARGN} expand the arguments after ex_target to a list
-    add_executable(${ex_target} ${ARGN})
     target_link_libraries(${ex_target} PRIVATE open62541cpp)
+
+    # force output directory to build/bin
+    set_target_properties(${ex_target}
+        PROPERTIES
+        ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
+        LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
+        RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
+        )
 
     include(../../Common.cmake)
     set_build_system_option(${ex_target})
